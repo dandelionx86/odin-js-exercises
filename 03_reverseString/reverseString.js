d0f55d859b389b1c13f63ebcd11591968e14392b
Dandelion <dandelionx86@gmail.com>
2021-11-24 17:00:37 -0600

    done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
+// this function takes a string and reverses its order
 const reverseString = function(str) {
+    // take a string that includes multiple words and grammar
+    // reverse the order of the string
+
+    // the split method separates each character into it's own object 
+    // the reverse method reverses the oder of the objects (characters) in the array
+    // the join method joins the objects (charactes) together in the reverse order
+    return str.split("").reverse("").join("");
 
 };
 
