cd801d24f3ca0f7e10bd407378cbe992a1b83aa2
Dandelion <dandelionx86@gmail.com>
2021-11-27 16:49:04 -0600

    done, but looked at solution for answer / very confused about the syntax

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  //[°C] = ([°F] − 32) × 5⁄9
+  
+  // 
+  return Math.round((f - 32) * (5/9) * 10) / 10;
+  
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  // [°F] = [°C] × 9⁄5 + 32 
+  
+  return Math.round(((c * 9/5) + 32) * 10) / 10;
+  
 };
 
 // Do not edit below this line
